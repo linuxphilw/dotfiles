ae7d652ad7c7564e871d9e2e813336cca84ff810
Phil W <coperni@lampbusters.com>
2017-02-08 17:20:41 -0700

    Split script up into three parts

--- dotfiles_symlinks_setup.sh
+++ dotfiles_symlinks_setup.sh
@@ -1,60 +1,12 @@
 #!/bin/bash
 
-#This script copies several dotfiles to a .dotfiles dir
-#Then replaces the originals with symlinks
+#This script sets up links to the dotfiles downloaded from github
+#You must run the remove and backup scripts first
 #The .dotfiles directory will then be maintained by git
-
-if [ ! -d $HOME/dotfiles ]; then
-    mkdir $HOME/dotfiles
-else
-    echo "$HOME/dotfiles already exists"
-fi
-
-if [ ! -d $HOME/dotfiles/.config ]; then
-    mkdir $HOME/dotfiles/.config
-else
-    echo "$HOME/dotfiles/.config already exists"
-fi
-
-#IF YOU KEEP ANY USERNAME PASSWORDS in your .ssh/config files
-#DO NOT BACK THEM UP TO GITHUB!
-
-if [ ! -d $HOME/dotfiles/.ssh ]; then
-    mkdir $HOME/dotfiles/.ssh ]
-else
-    echo "$HOME/dotfiles/.ssh already exists"
-fi
-
+sourcedir=dotfiles
 #listing files
 listoffiles=".vimrc .config/kcminputrc .ssh/config"
 
-for dots in $listoffiles; do
-    cp $HOME/$dots $HOME/dotfiles/$dots
-done
-
-#DANGEROUS PART
-echo "This next part is dangerous so make sure"
-echo "That all the files were copied to the .dotfiles"
-echo "directory before running this part"
-echo "do you want to continue? Select the number next to your choice"
-
-select choice in yes no; do
-    if [ $choice = "yes" ]; then
-        break
-    elif [ $choice = "no"; then
-        echo "make sure your files are in the .dotfiles dir"
-        echo "then rerun the script"
-        exit
-    else
-        echo "select the number next to your choice"
-    fi
-done
-
-
-for origfiles in $listoffiles; do
-    rm $HOME/$origfiles
-done
-
 for links in $listoffiles; do
-    ln -s $HOME/dotfiles/$links $HOME/$links
+    ln -s $HOME/$sourcedir/$links $HOME/$links
 done
---
+++ remove_olddotfiles.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#This script removes the old dotfiles, MAKE SURE TO RUN backup_orig_dots.sh first
+backupdir=".dotfiles_orig"
+sourcedir="dotfiles"
+if [ ! -d $HOME/$backupdir -o ! -d $HOME/$sourcedir ]; then
+    echo "MAKE SURE to run backup_orig_dots.sh BEFORE you run this script"
+	echo "If you changed the $backupdir in that script you need to make sure"
+	echo "to set it to the same path in THIS file"
+	exit
+fi
+
+#listing files
+listoffiles=".vimrc .config/kcminputrc .ssh/config"
+
+#DANGEROUS PART
+echo "This next part is dangerous so make sure"
+echo "That all the files were copied to the .dotfiles"
+echo "directory before running this part"
+echo "do you want to continue? Select the number next to your choice"
+
+select choice in yes no; do
+    if [ $choice = "yes" ]; then
+        break
+    elif [ $choice = "no" ]; then
+        echo "Note, for the final linking script to work, you will eventually have to do this part"
+        echo "When you are ready, re run this script and choose the numbe for yes "
+        exit
+    else
+        echo "select the number next to your choice"
+    fi
+done
+
+
+for origfiles in $listoffiles; do
+    rm $HOME/$origfiles
+done
+
